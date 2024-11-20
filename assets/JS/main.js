12dd5465f39d9f8d5156ac5da1c98c185ef42a39
yahyajaara <yahyajaara1411@gmail.com>
2024-11-20 18:58:57 +0200

    end edit

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -48,8 +48,6 @@ displayCategory();
 
 
 
-
-
 const timerr = ()=>
     {
         const ramadanDay = new Date("2025-03-01T00:00:00").getTime();
@@ -61,7 +59,6 @@ const timerr = ()=>
         const minutes = Math.floor(((distance % 86400000) % 3600000) / 60000);
         const seconds = Math.floor((((distance % 86400000) % 3600000) % 60000) / 1000);
 
-        console.log(ramadanDay)
 
         document.querySelector(".ramadan .row").innerHTML = 
         `
@@ -84,3 +81,181 @@ const timerr = ()=>
     setInterval(() => {
         timerr();
     }, 1000);
+
+
+
+
+    const getProdut = async (page) =>
+        {
+            const limit = 30 ;
+            const skip = (page - 1) * limit;
+            const {data} = await axios.get(`https://dummyjson.com/products?limit=30&skip=${skip}`);
+            return data ;
+        }
+        
+        const displayProduct = async (page = 1) =>
+        {
+            document.querySelector("._loding").classList.add("active");
+            
+            try
+            {
+                
+                const data = await getProdut(page);
+                const limit = 30 ;
+                const numberOfPages = Math.ceil(data.total / limit);
+                const result = data.products.map((product)=>
+                {
+                    return`
+                                 <div class = "aproduct">
+            
+                                        <img src = "${product.thumbnail} " class = 'imgess' /> 
+                                        <h3> ${product.title} </h3> 
+                                        <span>${product.price} </span>
+                                 </div>
+                    `
+                }
+                
+                ).join(" ");
+                
+                document.querySelector(".all_product .row").innerHTML = result;
+                document.querySelector("._loding").classList.remove("active");
+
+                let paginationJS = ``;
+
+                if (page == 1)
+                {
+                     paginationJS += `<li class="page-item"> <button class="page-link" >&laquo</button> </li>`;
+                }
+
+                else
+                {
+                     paginationJS += `<li class="page-item"> <button onclick=displayProduct(${page-1}) class="page-link" >&laquo</button> </li>`;
+                }
+               
+               
+
+               for(let i = 1; i <= numberOfPages; i++)
+                {
+                    paginationJS += `<li class="page-item${i==page?'active':''} "> <button onclick=displayProduct(${i}) class="page-link">${i}</button> </li>`;
+                }
+
+
+                if (page == numberOfPages)
+                {
+                    paginationJS += `<li class="page-item"> <button class="page-link">&raquo</button> </li>`;
+
+                }
+
+                else
+                {
+                    paginationJS += `<li class="page-item"> <button onclick=displayProduct(${parseInt(page)+1}) class="page-link">&raquo</button> </li>`;
+
+                }
+                
+
+                document.querySelector(".pagination").innerHTML = paginationJS;
+
+                myModal();
+
+            }
+        
+            catch(error)
+            {
+                document.querySelector("._loding").classList.add("active");
+            }
+        
+        }
+
+
+    displayProduct();
+
+    function myModal ()
+    {
+        const modal = document.querySelector(".my_modal");
+        const closeButton = document.querySelector(".closeButton");
+        const rightButton = document.querySelector(".rightButton");
+        const leftButton = document.querySelector(".leftButton");
+        const image = Array.from(document.querySelectorAll(".imgess"));
+        let indexOfImage = 0 ;
+    
+        image.forEach(function(img)
+            {
+                img.addEventListener("click" , function(e)
+                {
+                    modal.classList.remove("nonDisabled");
+                    const source = e.target;
+                    modal.querySelector('img').setAttribute("src", source.src);
+                    indexOfImage = image.indexOf(source);
+
+                });
+
+            }
+        );
+
+        closeButton.addEventListener("click" , function(e)
+            {
+                modal.classList.add("nonDisabled");
+            }
+
+        );
+
+
+        rightButton.addEventListener("click" , function(e)
+            {
+                indexOfImage++;
+                if(indexOfImage >= image.length)
+                {
+                    indexOfImage = 0 ;
+                }
+                modal.querySelector('img').setAttribute("src", image[indexOfImage].src);
+            }
+    );
+
+        leftButton.addEventListener("click" , function(e)
+            {
+                indexOfImage--;
+                if(indexOfImage < 0)
+                {
+                    indexOfImage = image.length - 1 ;
+                }
+                modal.querySelector('img').setAttribute("src", image[indexOfImage].src);
+            }
+
+    );
+
+        document.addEventListener("keydown" , function(e)
+                {
+                    console.log(e.key)
+                    
+                    if(e.code == "Escape")
+                    {
+                        modal.classList.add("nonDisabled");
+                    }
+
+                    else if(e.code == "ArrowRight")
+                    {
+                        indexOfImage++;
+                        if(indexOfImage >= image.length)
+                        {
+                            indexOfImage = 0 ;
+                        }
+                        modal.querySelector('img').setAttribute("src", image[indexOfImage].src);
+                    }
+
+
+                    else if(e.code == "ArrowLeft")
+                    {
+                        indexOfImage--;
+                        if(indexOfImage < 0)
+                        {
+                            indexOfImage = image.length - 1 ;
+                        }
+                        modal.querySelector('img').setAttribute("src", image[indexOfImage].src);
+                    }
+                }
+        );
+
+
+    }
+
+    
